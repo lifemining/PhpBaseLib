06579246fbf8531d4eb69ad0602a8b2927dff18d
nemo <nemoglobine@gmail.com>
2022-01-16 21:38:43 +0100

    test class exists add method getMethods

--- src/Tools/Object.php
+++ src/Tools/Object.php
@@ -6,13 +6,32 @@ use ReflectionClass;
 
 class Object
 {
-    public static function getConstantes ($class_name) {
-        $oClass = new ReflectionClass($class_name);
+    public static function exists ($className) {
+        if (!class_exists($className)) {
+            trigger_error($className.' is not loaded');
+        }
+    }
+    
+    public static function getConstantes ($className) {
+        self::exists($className);
+        $oClass = new ReflectionClass($className);
         return $oClass->getConstants();
     }
 
-    public static function hasMethod ($class_name, $method) {
-        $oClass = new ReflectionClass($class_name);
+    public static function hasMethod ($className, $method) {
+        self::exists($className);
+        $oClass = new ReflectionClass($className);
         return $oClass->hasMethod($method);
+    }
+
+    public static function getMethods ($className) {
+        self::exists($className);
+        $oClass     = new ReflectionClass($className);
+        $methods    = $oClass->getMethods();
+        $result     = array();
+        foreach ($methods as $method) {
+            $result[] = $method->name;
+        }
+        return $result;
     }
 }
